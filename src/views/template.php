91000e43147b58459482936650ab6b34513fbe9d
kayreddd <148087656+kayreddd@users.noreply.github.com>
2024-05-19 03:43:45 +0200

    création de la page contact presque terminé

--- src/views/template.php
+++ src/views/template.php
@@ -4,6 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
+        <link rel="stylesheet" href="<?=PROJECT_FOLDER ?>www/css/contact.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Staatliches&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/f09ae54942.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <?= $navbar ?>
