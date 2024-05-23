32e86ab148752dd0382ebb91c9a61079c9dec025
kayreddd <148087656+kayreddd@users.noreply.github.com>
2024-05-23 22:59:31 +0200

    ajout du footer pour les pages

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -3,7 +3,52 @@
 ob_start();?>
 
 <footer>
-    
+    <div class="all-foot1">
+        <div class="footer_part1">
+            <p>NOUS CONTACTER</p>
+            <ul>
+                <li>Tél : 06 98 20 99 87</li>
+                <li><a href="#">Email : sweethome78390@gmail.com</a></li>
+            </ul>
+        </div>
+        <div class="footer_part2">
+            <p>CONDITIONS</p>
+            <ul>
+                <li><a href="#">Paramètres de cookies</a></li>
+                <li><a href="#">Conditions d'utilisation</a></li>
+                <li><a href="#">Condition générales de ventes</a></li>
+                <li><a href="#">Politique de confidentialité et de cookies</a></li>
+                <li><a href="#">Mentions légales</a></li>
+            </ul>
+        </div>
+        <div class="footer_part3">
+            <img class="img_sweet_home" src="<?= PROJECT_FOLDER ?>/src/img/sweetHome.svg">
+        </div>
+    </div>
+    <div class="all-foot2">
+        <div class="second_footer">
+            <div class="logo_social">
+                <a href="https://www.instagram.com/">
+                    <i class="fa-brands fa-instagram" style="color: black;"></i>
+                </a>
+                <a href="https://www.snapchat.com/fr-FR">
+                    <i class="fa-brands fa-snapchat" style="color: black;"></i>
+                </a>
+                <a href="https://www.pinterest.fr/">
+                    <i class="fa-brands fa-pinterest" style="color: black;"></i>
+                </a>
+            </div>
+            <div class="text-mid">
+                <p>PAIEMENT 100% SÉCURISÉ <i class="fa-solid fa-circle-check"></i></p>
+            </div>
+            <div class="logo_payment">
+                <i class="fa-brands fa-cc-visa" style="color: black;"></i>
+                <i class="fa-brands fa-cc-mastercard" style="color: black;"></i>
+                <i class="fa-brands fa-cc-paypal" style="color: black;"></i>
+                <i class="fa-brands fa-cc-apple-pay" style="color: black;"></i>
+            </div>
+        </div>
+    </div>
 </footer>
 
 <?php
--- src/views/template.php
+++ src/views/template.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
         <link rel="stylesheet" href="<?=PROJECT_FOLDER ?>www/css/contact.css">
+        <link rel="stylesheet" href="<?=PROJECT_FOLDER ?>www/css/footer.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Staatliches&display=swap" rel="stylesheet">
