39882817ab6c0f4a4e555983623ee3c7b6afcef9
kayreddd <148087656+kayreddd@users.noreply.github.com>
2024-07-07 23:21:58 +0200

    ajout fonctionnalité quantité + partie de récap de la commande du client

--- src/views/pages/shopCart.php
+++ src/views/pages/shopCart.php
@@ -6,109 +6,187 @@ $title = "Sweet Home";
 require_once __DIR__ . '/../../utils/common.php';
 
 ob_start(); ?>
-
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
 <body>
     <main>
-        <div class="shopping-cart">
-            <!-- Title -->
-            <div class="title">
-                Shopping Bag
-            </div>
+        <div class="px-4 px-lg-0">
+            <div class="pb-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
 
-            <!-- Product #1 -->
-            <div class="item">
-                <div class="buttons">
-                    <span class="delete-btn"></span>
-                    <span class="like-btn"></span>
+                            <!-- Shopping cart table -->
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" class="border-0 bg-light">
+                                                <div class="p-2 px-3 text-uppercase">Produits</div>
+                                            </th>
+                                            <th scope="col" class="border-0 bg-light">
+                                                <div class="py-2 text-uppercase">Prix</div>
+                                            </th>
+                                            <th scope="col" class="border-0 bg-light">
+                                                <div class="py-2 text-uppercase">Quantité</div>
+                                            </th>
+                                            <th scope="col" class="border-0 bg-light">
+                                                <div class="py-2 text-uppercase">Supprimer</div>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row" class="border-0">
+                                                <div class="p-2">
+                                                    <img src="https://bootstrapious.com/i/snippets/sn-cart/product-1.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
+                                                    <div class="ml-3 d-inline-block align-middle">
+                                                        <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">Tarte aux fraises</a></h5><span class="text-muted font-weight-normal font-italic d-block">Catégorie: Pâtisseries</span>
+                                                    </div>
+                                                </div>
+                                            </th>
+                                            <td class="border-0 align-middle"><strong>3.10€</strong></td>
+                                            <td class="border-0 align-middle">
+                                                <div class="quantity">
+                                                    <button class="minus-btn" type="button" name="button">
+                                                        <i class="fa-solid fa-minus"></i>
+                                                        </button>
+                                                        <input type="text" name="name" value="1">
+                                                        <button class="plus-btn" type="button" name="button">
+                                                            <i class="fa-solid fa-plus"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                            <td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">
+                                                <div class="p-2">
+                                                    <img src="<?=PROJECT_FOLDER ?>src/images/no_image.svg" alt="" width="70" class="img-fluid rounded shadow-sm">
+                                                    <div class="ml-3 d-inline-block align-middle">
+                                                        <h5 class="mb-0"><a href="#" class="text-dark d-inline-block">Baguette tradition</a></h5><span class="text-muted font-weight-normal font-italic">Catégorie: Pains</span>
+                                                    </div>
+                                                </div>
+                                            </th>
+                                            <td class="align-middle"><strong>1.50€</strong></td>
+                                            <td class="align-middle">
+                                                <div class="quantity">
+                                                    <button class="minus-btn" type="button" name="button">
+                                                        <i class="fa-solid fa-minus"></i>
+                                                    </button>
+                                                    <input type="text" name="name" value="1">
+                                                    <button class="plus-btn" type="button" name="button">
+                                                        <i class="fa-solid fa-plus"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                            <td class="align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">
+                                                <div class="p-2">
+                                                    <img src="https://bootstrapious.com/i/snippets/sn-cart/product-3.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
+                                                    <div class="ml-3 d-inline-block align-middle">
+                                                        <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block">Macaron framboise</a></h5><span class="text-muted font-weight-normal font-italic">Catégorie: Pâtisseries</span>
+                                                    </div>
+                                                </div>
+                                            <td class="align-middle"><strong>3.00€</strong></td>
+                                            <td class="align-middle">
+                                                <div class="quantity">
+                                                    <button class="minus-btn" type="button" name="button">
+                                                        <i class="fa-solid fa-minus"></i>
+                                                    </button>
+                                                    <input type="text" name="name" value="1">
+                                                    <button class="plus-btn" type="button" name="button">
+                                                        <i class="fa-solid fa-plus"></i>
+                                                        
+                                                    </button>
+                                                </div>
+                                            </td>
+                                            <td class="align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- End -->
+                        </div>
+                    </div>
+
+                    <div class="row py-5 p-4 bg-white rounded shadow-sm">
+                        <div class="col-lg-6">
+                            <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Code de réduction</div>
+                            <div class="p-4">
+                                <p class="font-italic mb-4">Si vous avez un code promo, veuillez le saisir dans la case ci-dessous.</p>
+                                <div class="input-group mb-4 border rounded-pill p-2">
+                                    <input type="text" placeholder="Apply coupon" aria-describedby="button-addon3" class="form-control border-0">
+                                    <div class="input-group-append border-0">
+                                        <button id="button-addon3" type="button" class="btn btn-dark px-4 rounded-pill"><i class="fa fa-gift mr-2"></i>Ajoutez un code</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Instructions pour le vendeur</div>
+                            <div class="p-4">
+                                <p class="font-italic mb-4">Si vous avez des informations pour le vendeur, vous pouvez les laisser dans la case ci-dessous.</p>
+                                <textarea name="" cols="30" rows="2" class="form-control"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Récapitulatif de la commande </div>
+                            <div class="p-4">
+                                <p class="font-italic mb-4">Les frais d'expédition et les frais supplémentaires sont calculés en fonction des valeurs que vous avez saisies.</p>
+                                <ul class="list-unstyled mb-4">
+                                    <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Sous-total </strong><strong>7.60€</strong></li>
+                                    <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Frais de livraison</strong><strong>2.99€</strong></li>
+                                    <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Taxe</strong><strong>0.00€</strong></li>
+                                    <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total</strong>
+                                        <h5 class="font-weight-bold">10.59€</h5>
+                                    </li>
+                                </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block">Passer au paiement</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="image">
-                    <img src="item-1.png" alt="" />
-                </div>
-
-                <div class="description">
-                    <span> </span>
-                    <span>Tarte aux fraise</span>
-                    <span>Pâtisseries</span>
-                </div>
-
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button">
-                        <img src="plus.svg" alt="" />
-                    </button>
-                    <input type="text" name="name" value="1">
-                    <button class="minus-btn" type="button" name="button">
-                        <img src="minus.svg" alt="" />
-                    </button>
-                </div>
-
-                <div class="total-price">3.10€</div>
-            </div>
-
-            <!-- Product #2 -->
-            <div class="item">
-                <div class="buttons">
-                    <span class="delete-btn"></span>
-                    <span class="like-btn"></span>
-                </div>
-
-                <div class="image">
-                    <img src="item-2.png" alt="" />
-                </div>
-
-                <div class="description">
-                    <span> </span>
-                    <span>Macaron framboise</span>
-                    <span>Pâtisseries</span>
-                </div>
-
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button">
-                        <img src="plus.svg" alt="" />
-                    </button>
-                    <input type="text" name="name" value="1">
-                    <button class="minus-btn" type="button" name="button">
-                        <img src="minus.svg" alt="" />
-                    </button>
-                </div>
-
-                <div class="total-price">3.00€</div>
-            </div>
-
-            <!-- Product #3 -->
-            <div class="item">
-                <div class="buttons">
-                    <span class="delete-btn"></span>
-                    <span class="like-btn"></span>
-                </div>
-
-                <div class="image">
-                    <img src="item-3.png" alt="" />
-                </div>
-
-                <div class="description">
-                    <span> </span>
-                    <span>Baguette Tradition</span>
-                    <span>Pains</span>
-                </div>
-
-                <div class="quantity">
-                    <button class="plus-btn" type="button" name="button">
-                        <img src="plus.svg" alt="" />
-                    </button>
-                    <input type="text" name="name" value="1">
-                    <button class="minus-btn" type="button" name="button">
-                        <img src="minus.svg" alt="" />
-                    </button>
-                </div>
-
-                <div class="total-price">1.50€</div>
             </div>
         </div>
     </main>
-    <script>
 
-    </script>
 </body>
+<script>
+    // système de quantité plus et moins
+    $('.minus-btn').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $input = $this.closest('div').find('input');
+        var value = parseInt($input.val());
+
+        if (value > 1) {
+            value = value - 1;
+        } else {
+            value = 0;
+        }
+
+        $input.val(value);
+    });
+
+    $('.plus-btn').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $input = $this.closest('div').find('input');
+        var value = parseInt($input.val());
+
+        if (value < 100) {
+            value = value + 1;
+        } else {
+            value = 100;
+        }
+
+        $input.val(value);
+    });
+</script>
+
 
 <?php $page_content = ob_get_clean();
